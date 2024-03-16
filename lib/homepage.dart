4318c48cfc3316c6bae28b011718c0f775b11b1f
Auggie Bennett <auggie@thebennett.net>
2024-03-16 11:42:26 -0500

    gah

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:itsthevine/screens/desktop_body.dart';
-import 'package:itsthevine/screens/mobile_body.dart';
+import 'package:itsthevine/screens/desktop/desktop_home.dart';
+import 'package:itsthevine/screens/mobile/mobile_home.dart';
 import 'package:itsthevine/widgets/responsive_layout.dart';
 
 class HomePage extends StatefulWidget {
